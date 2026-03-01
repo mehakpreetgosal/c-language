08a2c7046eb7f5c740fbaa87debb4a9a85a1e915
Mehakpreet kaur <mehakpreetgosal@gmail.com>
2026-03-01 20:22:02 +0530

    practicing programs in c language

---
+++ EXERCISE.C
@@ -0,0 +1,9 @@
+  #include <stdio.h>
+  main() {
+  char gender;
+  int yos, qq, sal;
+
+  printf("Enter Gender , Years of services and Qualifications (0 = G, 1 = PG):");
+  scanf("%c %d %d", &gender, &yos ,&qq);
+
+  if(gender == M)
